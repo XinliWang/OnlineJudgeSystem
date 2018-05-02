8faa8f658228ed1db4a82c8b2f48bba2226f3f33
Xinli Wang <xinliwangnyu@gmail.com>
2018-05-02 18:53:33 -0400

    add problemlist and utilize bootstrap for better user experience

---
+++ oj-client/src/app/mock-problem.ts
@@ -0,0 +1,26 @@
+import { Problem } from "./models/problem.model"
+
+export const PROBLEMS: Problem[] = [{
+  id: 1,
+  name: "2sum",
+  desc: "Given an array of integers, return indices of the two numbers such that they add up to a specific target.You may assume that each input would have exactly one solution, and you may not use the same element twice.",
+  difficulty: "easy"
+},
+{
+  id: 2,
+  name: "3sum",
+  desc: "Given an array of integers, return indices of the two numbers such that they add up to a specific target.You may assume that each input would have exactly one solution, and you may not use the same element twice.",
+  difficulty: "medium"
+},
+{
+  id: 3,
+  name: "4sum",
+  desc: "Given an array of integers, return indices of the two numbers such that they add up to a specific target.You may assume that each input would have exactly one solution, and you may not use the same element twice.",
+  difficulty: "hard"
+},
+{
+  id: 4,
+  name: "2sumII",
+  desc: "Given an array of integers, return indices of the two numbers such that they add up to a specific target.You may assume that each input would have exactly one solution, and you may not use the same element twice.",
+  difficulty: "easy"
+}];
